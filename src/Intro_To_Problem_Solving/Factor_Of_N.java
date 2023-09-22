cc588746e5c7b48a0bebba8e212dbc9275534340
Sahin Islam <sahinislam878@gmail.com>
2023-09-22 11:51:31 +0530

    Adding Intor_To_ProblemSolving Classes Codes

---
+++ src/Intro_To_Problem_Solving/Factor_Of_N.java
@@ -0,0 +1,38 @@
+package Intro_To_Problem_Solving;
+
+import java.util.Scanner;
+
+public class Factor_Of_N {
+
+    public static int countfactor(int n){
+        int c = 0;
+        for (int i = 1;i<=n;i++){ // N Iterations
+            if (n%i == 0){
+                c++;
+            }
+        }
+        return c;
+    }
+
+    //Optimized This Above Code
+
+    public static int OtimizedCountFactor(int n){
+        int c = 0;
+
+        for (int i = 1; i*i < n;i++){ //SQRt of N Iterations
+            if(i == n/i){
+                c++;
+            }
+            else{
+                c+=2;
+            }
+        }
+        return c;
+    }
+    public static void main(String[] args) {
+        System.out.println(countfactor(10));
+        System.out.println(OtimizedCountFactor(12));
+
+    }
+
+}
