ce929259802b5991a5d2e2e0ac2ff431aa6192e4
sahin Islam <sahinislam878@gmail.com>
2025-11-21 00:12:16 +0530

    Sorting 1 : Given An Array A and L,M and R,where L to M is sorted and M + 1 to R is sorted.now sort the whole array

--- src/DSA_02/Sorting_01/MergeAandB.java
+++ src/DSA_02/Sorting_01/MergeAandB.java
@@ -37,7 +37,47 @@ public class MergeAandB {
         }
 
 
-        return ans;
+        return ans; // T.C : O(N+M), S.C :O(n+m)
+    }
+
+
+
+    /*
+    * Given An Array A and L,M and R,where L to M is sorted and M + 1 to R is sorted.now sort the whole array.
+    *
+    * */
+
+    public static int[] merge(int[] a,int L,int M,int R){
+        int n = a.length;
+        int[] ans = new int[n];
+
+
+        int p1 = L,p2 = M+1,i = 0;
+
+        while (p1 <= M && p2 <= R){
+            if(a[p1] < a[p2]){
+                ans[i] = a[p1];
+                p1++;
+                i++;
+
+            }
+            else {
+                ans[i] = a[p2];
+                p2++;
+                i++;
+            }
+        }
+        while (p1 <= M){
+            ans[i] = a[p1];
+            i++;
+            p1++;
+        }
+        while (p2 <= R){
+            ans[i] = a[p2];
+            i++;
+            p2++;
+        }
+        return ans; //T.C : O(N)
     }
 
     public static void main(String[] args) {
@@ -46,6 +86,13 @@ public class MergeAandB {
         int ans[] = mergeTwoSortedArray(a,b);
         for (int i = 0; i < ans.length; i++) {
             System.out.print(ans[i] + " ");
+        }
+
+
+        int[] a = {1,2,3,5,7,99,111,222,2,3,10,12,55,66,69,89,222,333};
+        int[] ans2 = merge(a,0,7,a.length-1);
+        for (int i = 0; i < ans2.length; i++) {
+            System.out.print(ans2[i] + " ");
         }
     }
 }
