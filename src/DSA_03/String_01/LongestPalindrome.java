97283972aaf96a8509cc029c9f2d30b5d6642573
sahin Islam <sahinislam878@gmail.com>
2025-11-23 03:10:46 +0530

    String 1 : Length of longest Palindrome String - Optimal with some error

--- src/DSA_03/String_01/LongestPalindrome.java
+++ src/DSA_03/String_01/LongestPalindrome.java
@@ -16,7 +16,69 @@ public class LongestPalindrome {
         return ans; // T.C : O(n^3), S.C: O(1)
     }
 
+    //Optimization
+
+    /*
+
+    Idea: Every Palindrome has a center, find the center and iterate the left and right and check.
+
+    For given center find the longest Palindrome,
+    divide the problem into two parts,
+    i) OddLengthPalindrome
+    ii)evenLengthPalindrome
+
+    then find the max palindrome
+    */
+
+    public static int longestPalindromicSubString(String str){
+        int evenLength = maxLengthEvenPalindrome(str);
+        int oddLength = maxLengthOddPalindrome(str);
+        return Math.max(evenLength,oddLength);
+    }
+
+    private static int maxLengthOddPalindrome(String str) {
+        int n = str.length();
+        int maxOddPalindromeLength = 0;
+        for(int c = 0; c < n; c++){
+            int p1 = c;
+            int p2 = c;
+            while (p1 >= 0 && p2 < n){
+                if (str.charAt(p1) == str.charAt(p2)){
+                    p1--;
+                    p2++;
+                }else {
+                    break;
+                }
+            }
+            int currentMaxLength = p2 - p1 - 1;
+            maxOddPalindromeLength = Math.max(maxOddPalindromeLength,currentMaxLength);
+        }
+        return maxOddPalindromeLength;
+    }
+
+    private static int maxLengthEvenPalindrome(String str) {
+        int n = str.length();
+        int maxEvenPalindromeLength = 0;
+        for(int c = 0; c < n; c++){
+            int p1 = c;
+            int p2 = c + 1;
+            while (p1 >= 0 && p2 < n){
+                if (str.charAt(p1) == str.charAt(p2)){
+                    p1--;
+                    p2++;
+                }else {
+                    break;
+                }
+            }
+            int currentMaxLength = p2 - p1 + 1;
+            maxEvenPalindromeLength = Math.max(maxEvenPalindromeLength,currentMaxLength);
+        }
+        return maxEvenPalindromeLength;
+    }
+
     public static void main(String[] args) {
         System.out.println(bruteforce("dabba"));
+
+        System.out.println(longestPalindromicSubString("abyzzybx"));
     }
 }
