c0daee2658a04a5db9e056714a1777daa8ba246c
sahin Islam <sahinislam878@gmail.com>
2025-11-22 13:39:07 +0530

    String 1 : Length of longest Palindrome String - Bruteforce

--- src/String_01/LongestPalindrome.java
+++ src/String_01/LongestPalindrome.java
@@ -1,4 +1,20 @@
 package String_01;
 
-public class Longestpalindrome {
+public class LongestPalindrome {
+    public static int bruteforce(String str){
+        int ans = 0;
+        int n = str.length();
+        for (int i = 0; i < n; i++){
+            for (int j = i; j < n; j++){
+                if (isPalindrome.isPalindrome(str,i,j)){
+                    ans = Math.max(ans,j-i+1);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(bruteforce("dabba"));
+    }
 }
